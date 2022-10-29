f620e5fdc3ef673a9515314efe56874317427695
sooraj <sooraj@mozilor.com>
2022-10-29 18:18:00 +0530

    init

---
+++ src/component/GameLetGo.svelte
@@ -0,0 +1,24 @@
+<script>
+  import PreloadScene from "../game/PreloadScene";
+  import PlayScene from "../game/PlayScene";
+
+  /** @type {Phaser.Types.Core.GameConfig} */
+  const gameConfig = {
+    type: Phaser.AUTO,
+    width: 1000,
+    height: 340,
+    pixelArt: true,
+    transparent: true,
+    physics: {
+      default: "arcade",
+      arcade: {
+        debug: false,
+      },
+    },
+    parent: "game",
+    scene: [PreloadScene, PlayScene],
+  };
+  const game = new Phaser.Game(gameConfig);
+</script>
+
+<div id="game" />
